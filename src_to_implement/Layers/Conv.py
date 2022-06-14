63a5f1231dec6ecb3903ef3d2079946b2cfd5356
dhirajpoddar <dhirajpodar@gmail.com>
2022-06-14 15:17:20 +0200

    pooling implemented

--- src_to_implement/Layers/Conv.py
+++ src_to_implement/Layers/Conv.py
@@ -1,5 +1,11 @@
-class Conv:
+import numpy as np
+from Layers.Base import BaseLayer
+
+
+class Conv(BaseLayer):
     def __init__(self, stride_shape, convolution_shape, num_kernels):
+        super().__init__()
+        self.trainable = True
         self.stride_shape = stride_shape
         self.convolution_shape = convolution_shape
         self.num_kernels = num_kernels
